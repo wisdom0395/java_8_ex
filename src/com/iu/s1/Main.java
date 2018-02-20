015397e604057f32f62b4f2acd6af0c23b02ba60
seulgi <chae9912@naver.com>
2018-02-20 10:23:46 +0900

    Test1
    Program Start +

--- src/com/iu/s1/Main.java
+++ src/com/iu/s1/Main.java
@@ -3,8 +3,12 @@ package com.iu.s1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("Program Start");
+		
+		
+		
+		
 	}
 
 }
